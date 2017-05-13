a3faa9ff63e50940015ae38576aec6b01ed0e4bf
Lucas de Oliveira Silva <lucasxm21@hotmail.com>
2017-05-13 13:26:25 -0300

    Pequenas mudanças

--- combat.asm
+++ combat.asm
@@ -1,30 +1,95 @@
 .386
-title combat
 .model flat, stdcall
 option casemap :none
 
 include combat.inc
 
-point struct
-    x  DB ?
-    y  DB ?
-point ends
-
-player struct 
-    pos point <>
-    direc DB ?
-    life DB ?
-player ends
-
 .data
     player1 player <<?, ?>, 3, 100>
     player2 player <<?, ?>, 7, 100>
 
-    score point <0, 0>
+    score pair <0, 0>
 
 .data?
 
-.code
+.code 
 start:
+
+invoke GetModuleHandle, NULL
+mov hInstance, eax
+
+invoke WinMain, hInstance, NULL, NULL, SW_SHOWDEFAULT
+invoke ExitProcess, eax
+
+WinMain proc hInst:HINSTANCE, hPrevInst:HINSTANCE, CmdLine:LPSTR, CmdShow:DWORD
+	LOCAL wc:WNDCLASSEX                                            
+    LOCAL msg:MSG 
+    LOCAL hwnd:HWND
+
+	;Fill values in members of wc
+    mov wc.cbSize, SIZEOF WNDCLASSEX  
+    mov wc.style, CS_HREDRAW or CS_VREDRAW 
+    mov wc.lpfnWndProc, OFFSET WndProc 
+    mov wc.cbClsExtra, NULL 
+    mov wc.cbWndExtra, NULL 
+
+    push hInstance 
+    pop wc.hInstance 
+
+    mov wc.hbrBackground, COLOR_WINDOW + 1 
+    mov wc.lpszMenuName, NULL 
+    mov wc.lpszClassName, OFFSET ClassName 
+
+    invoke LoadIcon, NULL, IDI_APPLICATION 
+    mov wc.hIcon, eax 
+    mov wc.hIconSm, eax
+
+    invoke LoadCursor, NULL, IDC_ARROW 
+    mov wc.hCursor, eax 
+
+    invoke RegisterClassEx, addr wc ;Register our window class 
+
+    invoke CreateWindowEx, NULL,\ 
+                ADDR ClassName,\ 
+                ADDR AppName,\ 
+                WS_OVERLAPPEDWINDOW,\ 
+                CW_USEDEFAULT,\ 
+                CW_USEDEFAULT,\ 
+                CW_USEDEFAULT,\ 
+                CW_USEDEFAULT,\ 
+                NULL,\ 
+                NULL,\ 
+                hInst,\ 
+                NULL 
+
+    mov hwnd, eax 
+    invoke ShowWindow, hwnd, CmdShow ;Display our window on desktop 
+    invoke UpdateWindow, hwnd ;Refresh the client area
+
+    ;Enter message loop
+	.WHILE TRUE  
+        invoke GetMessage, ADDR msg, NULL, 0, 0 
+        .BREAK .IF (!eax) 
+
+        invoke TranslateMessage, ADDR msg 
+        invoke DispatchMessage, ADDR msg 
+	.ENDW 
+
+    mov eax, msg.wParam ;Return exit code in eax 
+    ret 
+WinMain endp
+
+WndProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM 
+    .IF uMsg == WM_DESTROY ;If the user closes our window 
+        invoke PostQuitMessage, NULL ;Quit our application 
+    .ELSE 
+        invoke DefWindowProc, hWnd, uMsg, wParam, lParam ;Default message processing 
+        ret 
+    .ENDIF 
+
+    xor eax, eax 
+    ret 
+WndProc endp
+
 end start
 
